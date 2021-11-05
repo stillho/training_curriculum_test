e496967619819b425643fe9a8f57d29e745d1908
stillho <yangdehuadao@gmail.com>
2021-11-05 16:18:18 +0900

    issue6(4)

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,14 +34,7 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-
-
-      wday_num = x
-      if  wday_num >= 7
-       wday_num = wday_num -7
-
-      end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: %w((日) (月) (火) (水) (木) (金) (土))[(@todays_date + x).wday] }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays: wdays [(@todays_date + x).wday] }
 
       @week_days.push(days)
     end
